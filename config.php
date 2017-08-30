ff05f3b208db902bf0a57dbf6b57e6d74da7eec5
smpascua <smpascua@gmail.com>
2017-08-30 18:49:34 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once(dirname(FILE).'/arc/ARC2.php'); path to the file ARC2.php
+
+SQL database configuration for storing the postings:
+$arc_config = array(
+/* MySQL database settings */
+'db_host' => 'localhost',
+'db_user' => 'root',
+'db_pwd' => 'admin',
+'db_name' => 'arc2test',
+
+/* ARC2 store settings */
+'store_name' => 'sandbox',
+
+/* SPARQL endpoint settings */
+'endpoint_features' => array(
+'select', 'construct', 'ask', 'describe', allow read
+'load', 'insert', 'delete', allow update
+'dump' allow backup
+),
+'endpoint_timeout' => 60, /* not implemented in ARC2 preview */
+'endpoint_read_key' => '', /* optional */
+'endpoint_write_key' => '', /* optional */
+'endpoint_max_limit' => 250, /* optional */
+);
